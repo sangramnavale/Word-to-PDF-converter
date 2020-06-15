ae2eec8c61d9a2c48ce61b60601ce11f0267f4da
Sangram Navale <sangramn41@gmail.com>
2020-06-15 11:36:49 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
-from docx2pdf import convert // First install "docx2pdf" module from pip, else this program will not work.
+from docx2pdf import convert
+# First install "docx2pdf" module from pip, else this program will not work.
 
 convert(r"C:\Users\user\Desktop\test.docx", r"C:\Users\user\Desktop\test.pdf") 
-// Specify the path where your word doc is located. 
-// Note: The original document was in "docx" format, one can also use for doc format.
+
+# Specify the path where your word doc is located. 
+# Note: The original document was in "docx" format, one can also use for doc format.
